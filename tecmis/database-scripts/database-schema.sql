081ce1c8565f84114ee57c7d1a5522b6da7f2def
Razim Mohamed <168627373+razim2003@users.noreply.github.com>
2026-04-08 16:26:10 +0530

    Create tecmis_java database with utf8mb4 charset

--- tecmis/database-scripts/database-schema.sql
+++ tecmis/database-scripts/database-schema.sql
@@ -1,3 +1,8 @@
+CREATE DATABASE IF NOT EXISTS tecmis_java
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE tecmis_java;
 
 CREATE TABLE user (
                       id CHAR(12) PRIMARY KEY,
