fbeab28c490da3851ac0cca93fce1491e833e55e
razim2003 <raazim2003@gmail.com>
2026-04-09 14:32:45 +0530

    added db connection

---
+++ tecmis/src/main/java/com/runtimex/tecmis/dao/impl/MarksDaoImpl.java
@@ -0,0 +1,9 @@
+package com.runtimex.tecmis.dao.impl;
+
+import com.runtimex.tecmis.dao.MarksDao;
+
+import java.sql.Connection;
+
+public class MarksDaoImpl implements MarksDao {
+    private final Connection connection;
+}
