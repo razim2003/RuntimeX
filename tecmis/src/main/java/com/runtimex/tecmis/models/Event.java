c5103df7a5185629ea23cf2f8becc75cf407cc5e
kayaththiry1776 <moorthykayu2@gmail.com>
2026-04-12 20:59:17 +0530

    added EventModel

---
+++ tecmis/src/main/java/com/runtimex/tecmis/models/Event.java
@@ -0,0 +1,62 @@
+package com.runtimex.tecmis.models;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Event {
+
+    private String eventId;
+    private String userId;
+    private String title;
+    private String description;
+    private LocalDate date;
+    private LocalTime time;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+}
