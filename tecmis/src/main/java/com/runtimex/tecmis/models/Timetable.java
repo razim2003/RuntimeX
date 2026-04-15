3529ef3eb4e790200784d3645fd116921c993cf0
rishma1710 <rishmafasloon04@gmail.com>
2026-04-16 00:31:10 +0530

    Timetable added

---
+++ tecmis/src/main/java/com/runtimex/tecmis/models/Timetable.java
@@ -0,0 +1,82 @@
+package com.runtimex.tecmis.models;
+
+public class Timetable {
+
+    private String timetableId;
+    private String adminId;
+    private String lecturerId;
+    private String courseCode;
+    private String location;
+    private int level;
+    private String type;
+    private int hours;
+
+
+    public Timetable() {}
+
+
+
+    public String getTimetableId() {
+        return timetableId;
+    }
+
+    public void setTimetableId(String timetableId) {
+        this.timetableId = timetableId;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getLecturerId() {
+        return lecturerId;
+    }
+
+    public void setLecturerId(String lecturerId) {
+        this.lecturerId = lecturerId;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+}
